2643824f8a223820e79f7c789c87f53ef0e68152
unfollowhailey <manantsoaranjha@gmail.com>
2026-02-09 16:48:12 +0300

    OKEY

--- app/views/login.php
+++ app/views/login.php
@@ -55,34 +55,6 @@
 </div>
 
     <script src="assets/js/script.js"></script>
-
-
-       <script>
-        document.addEventListener('DOMContentLoaded', () => {
-  console.log('DOM loaded');
-  
-  const signUpButton = document.getElementById('signUp');
-  const signInButton = document.getElementById('signIn');
-  const container = document.getElementById('container');
-
-  console.log('signUpButton:', signUpButton);
-  console.log('signInButton:', signInButton);
-  console.log('container:', container);
-
-  if (signUpButton && signInButton && container) {
-    signUpButton.addEventListener('click', () => {
-      console.log('Sign Up clicked');
-      container.classList.add("right-panel-active");
-    });
-
-    signInButton.addEventListener('click', () => {
-      console.log('Sign In clicked');
-      container.classList.remove("right-panel-active");
-    });
-  } else {
-    console.log('Elements not found');
-  }
-});
-       </script>
+    
 </body>
 </html>
