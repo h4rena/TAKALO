cbcf5e03525a885c480fc9d58c00ca97a89351d2
H4rena <h4renarakotoarison@gmail.com>
2026-02-09 16:08:43 +0300

    test

--- data/table.sql
+++ data/table.sql
@@ -2,4 +2,27 @@ CREATE DATABASE takalo;
 
 USE takalo;
 
+CREATE TABLE user_role_takalo (
+    role_id INT AUTO_INCREMENT PRIMARY KEY,
+    role VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE user_takalo (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    id_role INT NOT NULL,
+    username VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    hashedpassword VARCHAR(255) NOT NULL,
+    FOREIGN KEY (id_role) REFERENCES user_role_takalo(role_id)
+);
+
+CREATE TABLE objet_takalo (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    id_owner INT NOT NULL,
+    nom_objet VARCHAR(255) NOT NULL,
+    description TEXT,
+    image VARCHAR(255) NOT NULL,
+    price DECIMAL(10, 2) NOT NULL,
+    FOREIGN KEY (id_owner) REFERENCES user_takalo(id)
+);
 
